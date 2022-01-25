71902d53762e8ea013d72208e5bbb22fa89e273d
Manik Roy <94542466+Manik515@users.noreply.github.com>
2022-01-25 17:01:37 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<html>
+	<head>
+		<Title>Hospital Managemant System</Title>
+		<style>
+				.container{
+					background: url(images/hospital.jpg);
+					width: 100%;
+					min-height: 100vh;
+					background-size: cover;
+					background-attachment: fixed;
+				}
+				.header{
+				   border-radius: 10px;
+				   text-align: center;
+				   padding: 3px;
+				   margin-bottom: 40px;
+				   background-color: blueviolet; 
+			    }
+				.header h1{
+					font-size: 30px;
+					color: black;
+				}
+				.main ul{
+					margin-left: 50px;
+				}
+				.main ul li{
+					list-style-type: square;
+					color: gray;
+				}
+				.main ul li a{
+					text-decoration: none;
+					font-size: 30px;
+					padding: 10px
+				}
+				.main ul li a:hover{
+					text-decoration: underline;
+					color: black;
+				}
+			
+			
+		</style>
+	</head>
+
+	<body class="head">
+		<div class= container>
+			<div class="header">
+		        <h1>Welcome to Hospital Managemant System</h1>				
+			</div>
+
+			<div class="main">
+				<ul>
+					<li><a href="accountant.php">ACCOUNTANT</a></li>
+					<li><a href="RECIEPTIONIST.php">RECIEPTIONIST</a></li>
+					<li><a href="DOCTOR.php">DOCTOR</a></li>
+					<li><a href="NURSE.php">NURSE</a></li>
+					<li><a href="SPECIALIZATION.php">SPECIALIZATION</a></li>
+					<li><a href="EMPLOYEE.php">EMPLOYEE</a></li>
+					<li><a href="mainpage.php">DEPARTMENT</a></li>
+					<li><a href="mainpage.php">ROOM</a></li>
+					<li><a href="mainpage.php">DIAGNOSIS</a></li>
+					<li><a href="mainpage.php">PATIENT</a></li>
+					<li><a href="mainpage.php">NURSING</a></li>
+					<li><a href="mainpage.php">BILL</a></li>
+					<li><a href="mainpage.php">DISCHARGE</a></li>
+				</ul>
+			</div>
+            
+		</div>
+
+	</body>
+</html>
